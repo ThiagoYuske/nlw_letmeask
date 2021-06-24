06068010da54e5a8513184fbf194680b4d043f71
ThiagoYuxke <thiagoyuske@gmail.com>
2021-06-24 00:44:50 -0300

    revert teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 import { Home } from './pages/home';
 
 function App() {
   return (
     <Home />
     
+=======
+function App() {
+  return (
+    <h1>Hello World</h1>
+>>>>>>> parent of 85f4f72... Configurando o ambiente
   );
 }
 
